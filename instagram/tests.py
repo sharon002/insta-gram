d6641c0667f09bd638188c86121d1e374dbfca93
sharon002 <sharonjep2016@gmail.com>
2021-07-12 08:14:10 +0300

    add license to README.md

--- instagram/tests.py
+++ instagram/tests.py
@@ -5,12 +5,12 @@ class ImageTestClass(TestCase):
         self.image_name = Image(image_name='Farming',image_caption='Farming is the backbone of our economy')
     def test_instance(self):
         self.assertTrue(isinstance(self.image_name,Image))
-    # def test_save_image(self):
-    #     self.post.save_image()
-    #     images = Image.objects.all()
-    #     self.assertTrue(len(images)>0)
-# class CommentTestClass(TestCase):
-#     def setUp(self):
-#         self.comment=Comment(comment='nice')
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.comment,Comment))
+    def test_save_image(self):
+        self.post.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images)>0)
+class CommentTestClass(TestCase):
+    def setUp(self):
+        self.comment=Comment(comment='nice')
+    def test_instance(self):
+        self.assertTrue(isinstance(self.comment,Comment))
